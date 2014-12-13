1a7d1f357a6c85452f76440286e631fb4536a5c2
poohound <joshua@mensink.name>
2014-12-13 15:46:30 -0700

    Added Email Deleted Extra files and Added pictures to Bsis and Mism Pages.

---
+++ MVC Project/Controllers/EmailController.cs
@@ -0,0 +1,63 @@
+﻿using MVC_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class EmailController : Controller
+    {
+        //
+        // GET: /MailMessaging/
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Sent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact( Email objModelMail, HttpPostedFileBase fileUploader)
+        {
+            if (ModelState.IsValid)
+            {
+                string from = "test@jamesonricks.com";
+                using (MailMessage mail = new MailMessage( objModelMail.From, from))
+            {
+        mail.Subject = objModelMail.Subject;
+        mail.Body = objModelMail.Body;
+            if (fileUploader != null)
+            {
+              string fileName = Path.GetFileName(fileUploader.FileName);
+              mail.Attachments.Add(new Attachment(fileUploader.InputStream, fileName));
+            }
+        mail.IsBodyHtml = false;
+        SmtpClient smtp = new SmtpClient();
+        smtp.Host = "mail.jamesonricks.com";
+        smtp.EnableSsl = false;
+        NetworkCredential networkCredential = new NetworkCredential(from, "jameson4president");
+        smtp.UseDefaultCredentials = true;
+        smtp.Credentials = networkCredential;
+        smtp.Port = 26;
+
+        smtp.Send(mail);
+        ViewBag.Message = "Sent";
+        return View("Sent", objModelMail);
+        }
+        }
+        else
+        {
+        return View();
+        }
+        }
+
+}
+}
---
+++ MVC Project/Models/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project.Models
+{
+    public class Email
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string From { get; set; }
+    }
+}
